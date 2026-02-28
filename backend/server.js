57b732ce5ca06713494f6523568ec3556bc9d46b
Aathish101 <aathishvs2@gmail.com>
2026-02-28 12:01:14 +0530

    Updated Firebase admin to use env variables

--- backend/server.js
+++ backend/server.js
@@ -64,18 +64,24 @@ import { readFile } from "fs/promises";
    🔥 FIREBASE ADMIN INIT
 ========================= */
 
-
-const serviceAccountPath = "/etc/secrets/serviceAccountKey.json";
 try {
-  const serviceAccount = JSON.parse(
-    await readFile(serviceAccountPath, "utf-8")
-  );
+  if (
+    process.env.FIREBASE_PROJECT_ID &&
+    process.env.FIREBASE_CLIENT_EMAIL &&
+    process.env.FIREBASE_PRIVATE_KEY
+  ) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      }),
+    });
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-
-  console.log("🔥 Firebase Admin Initialized");
+    console.log("🔥 Firebase Admin Initialized Successfully");
+  } else {
+    throw new Error("Missing Firebase environment variables");
+  }
 } catch (error) {
   console.warn("⚠️ Firebase Admin NOT initialized.");
   console.warn(error.message);
